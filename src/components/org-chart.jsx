fb9e080949f54a3526436cb6f8135cba6889a92d
Amber Nguyen <ambernguyen@usf.edu>
2024-05-20 15:26:47 -0400

    put back the name and the job_id on the node

--- src/components/org-chart.jsx
+++ src/components/org-chart.jsx
@@ -71,10 +71,10 @@ const OrgChartComponent = () => {
               return `
                 <div style='width:${d.width}px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
                   <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
-                    <div style="display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${d.data.id}</div>
+                    <div style="display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">${d.data.job_id}</div>
                     <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
                     <div style="margin-top:${-imageDiffVert - 20}px;"><img src="${d.data.image}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
-                    <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">${d.data.job_id}</div>
+                    <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">${d.data.name}</div>
                     <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;">${d.data.position}</div>
                   </div>
                 </div>
